d7e44a104c0211aa645db93f0953f909dddf23e5
Wayne Chen <wayne.chen@mile.cloud>
2024-03-12 05:44:54 +0000

    fix: replace aead encrypt function with decrypt function

--- includes/func_aead.js
+++ includes/func_aead.js
@@ -6,25 +6,24 @@ const {
     first_level_key
 } = require("includes/env_variables.js");
 
-
 // Do not change any variables below
 const kms = kms_resource_name
 const aead_base64_str = first_level_key
 const binary_data = Buffer.from(aead_base64_str, 'base64');
 const hexed_string = "\\x" + binary_data.toString('hex').match(/.{1,2}/g).join("\\x");
 
-function deterministic_encrypt(column, kms_resource_name = kms, hex_string = hexed_string) {
-    // Encrypt the column
-    return `DETERMINISTIC_ENCRYPT(
+function deterministic_decrypt(column, kms_resource_name = kms, hex_string = hexed_string) {
+    // Decrypt the column
+    return `DETERMINISTIC_DECRYPT_STRING(
 		KEYS.KEYSET_CHAIN(
 			"${kms_resource_name}",
 	    b"${hex_string}"
     ),
-    CAST(${column} AS STRING),
+    ${column},
     ""
   )`;
 }
 
 module.exports = {
-    deterministic_encrypt,
+    deterministic_decrypt,
 };
